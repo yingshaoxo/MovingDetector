f580e1769ffe21433091c42ae0ab59bb1557bfdf
yingshaoxo <yingshaoxo@gmail.com>
2018-05-06 11:22:29 +0800

    imporve

--- main.py
+++ main.py
@@ -10,20 +10,24 @@ try:
 except Exception as e:
     print(e)
 
+
 class MovingDetector():
     def __init__(self):
         pass
 
-    def camera_detect(self, device=0):
+    def camera_detect(self, device=0, func=None):
         self.camera = cv2.VideoCapture(device)
+        self.width = int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
         if not self.camera.isOpened():
             print("Could not open camera")
             sys.exit()
 
-        history = 10#20    # 训练帧数
+        history = 10  # 20    # 训练帧数
 
-        bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)  # 背景减除器，设置阴影检测
+        bs = cv2.createBackgroundSubtractorKNN(
+            detectShadows=True)  # 背景减除器，设置阴影检测
         #bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)
         bs.setHistory(history)
 
@@ -43,25 +47,34 @@ class MovingDetector():
 
             # 对原始帧进行膨胀去噪
             th = cv2.threshold(fg_mask.copy(), 244, 255, cv2.THRESH_BINARY)[1]
-            th = cv2.erode(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
-            dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 3)), iterations=2)
+            th = cv2.erode(th, cv2.getStructuringElement(
+                cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
+            dilated = cv2.dilate(th, cv2.getStructuringElement(
+                cv2.MORPH_ELLIPSE, (8, 3)), iterations=2)
             # 获取所有检测框
-            image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
- 
+            image, contours, hier = cv2.findContours(
+                dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
             if len(contours) != 0:
                 # find the biggest area
-                c = max(contours, key=cv2.contourArea) #c = min(contours, key=cv2.contourArea)
+                # c = min(contours, key=cv2.contourArea)
+                c = max(contours, key=cv2.contourArea)
                 # shrink area
                 rect = cv2.minAreaRect(c)
                 box = cv2.boxPoints(rect)
                 # get center point
                 x = int(sum([point[0] for point in box])/4)
                 y = int(sum([point[1] for point in box])/4)
-                print("the center point is: ({x}, {y})".format(x=x, y=y))
                 # convert all coordinates floating point values to int
                 box = np.int0(box)
                 # draw a red 'nghien' rectangle
                 cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
+                # run comstomer function
+                if func != None:
+                    func({"info": {"width": self.width, "height": self.height},
+                          "center_point": (x, y), "box": box})
+                else:
+                    print("the center point is: ({x}, {y})".format(x=x, y=y))
 
             cv2.imshow("detection", frame)
             cv2.imshow("back", dilated)
@@ -70,11 +83,12 @@ class MovingDetector():
                 break
 
     def screen_detect(self, record_box_size=600):
-        record_box_size = record_box_size//2
+        record_box_size = record_box_size // 2
 
         history = 10   # 训练帧数
 
-        bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)   # 背景减除器，设置阴影检测
+        bs = cv2.createBackgroundSubtractorKNN(
+            detectShadows=True)   # 背景减除器，设置阴影检测
         bs.setHistory(history)
 
         frames = 0
@@ -88,7 +102,8 @@ class MovingDetector():
             right_bottom_x = mouse_x + record_box_size
             right_bottom_y = mouse_y + record_box_size
 
-            frame =  np.array(ImageGrab.grab(bbox=(left_top_x, left_top_y, right_bottom_x, right_bottom_y)))
+            frame = np.array(ImageGrab.grab(
+                bbox=(left_top_x, left_top_y, right_bottom_x, right_bottom_y)))
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
             fg_mask = bs.apply(frame)   # 获取 foreground mask
@@ -99,11 +114,14 @@ class MovingDetector():
 
             # 对原始帧进行膨胀去噪
             th = cv2.threshold(fg_mask.copy(), 244, 255, cv2.THRESH_BINARY)[1]
-            th = cv2.erode(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
-            dilated = cv2.dilate(th, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (8, 3)), iterations=2)
+            th = cv2.erode(th, cv2.getStructuringElement(
+                cv2.MORPH_ELLIPSE, (3, 3)), iterations=2)
+            dilated = cv2.dilate(th, cv2.getStructuringElement(
+                cv2.MORPH_ELLIPSE, (8, 3)), iterations=2)
             # 获取所有检测框
-            image, contours, hier = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
- 
+            image, contours, hier = cv2.findContours(
+                dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
             if len(contours) != 0:
                 # find the biggest area
                 c = max(contours, key=cv2.contourArea)
@@ -130,6 +148,26 @@ class MovingDetector():
             if cv2.waitKey(110) & 0xff == 27:
                 break
 
+
+def func(return_values):
+    camera_info = return_values["info"]
+    width = camera_info["width"]
+    half_width = width / 2
+
+    middle_width = 0.3 * width
+
+    first_line = half_width - middle_width/2
+    second_line = half_width + middle_width/2
+
+    x = return_values["center_point"][0]
+    if x < first_line:
+        print("left")
+    elif first_line < x < second_line:
+        print("middle")
+    elif x > second_line:
+        print("right")
+
+
 if __name__ == '__main__':
     detector = MovingDetector()
-    detector.camera_detect(1)
+    detector.camera_detect(device=1, func=func)
